1efc1eb3164c07580911616c2144e5cb7b2169d0
Omnimind <omnimind.sg@gmail.com>
2024-04-26 18:04:34 +0800

    feat: add documentation comments.

--- lib/mogua.dart
+++ lib/mogua.dart
@@ -1,11 +1,18 @@
 import 'mogua_platform_interface.dart';
 
 class Mogua {
+  /// Initialize Mogua.
+  /// [appKey] The App Key associated with your application, you can find it on the mogua.io dashboard.
+  /// [allowClipboardAccess] Whether to allow access to the clipboard.
+  /// Enabling [allowClipboardAccess] can enhance accuracy, but may trigger permission warnings.
   static Future<void> init(
           {required String appKey, bool allowClipboardAccess = true}) =>
       MoguaPlatform.instance
           .init(appKey: appKey, allowClipboardAccess: allowClipboardAccess);
 
+  /// Retrieves data from the Mogua platform.
+  /// Returns the data asynchronously, containing the parameters received from the web.
+  /// This data is cached, ensuring identical return values for subsequent calls to [getData].
   static Future<Map<String, dynamic>> getData() =>
       MoguaPlatform.instance.getData();
 }
